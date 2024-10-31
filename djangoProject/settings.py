0f451e2a0e0ab3a4c9cf6caa4945f0941698420d
Zahra Roshani <zahraroshani973@gmail.com>
2024-10-31 20:52:20 +0330

    change logo,add requierments.txt

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -123,8 +123,10 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS=[
     os.path.join(BASE_DIR,'assets')
 ]
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 MEDIA_URL = '/media/'
 MEDIA_ROOT=os.path.join(BASE_DIR,'media/')
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
